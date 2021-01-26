b7941892e6ab7a790bf467513ba146c8fb08d09d
gabrielkollum <58408268+gabrielkollum@users.noreply.github.com>
2021-01-26 16:08:05 +0000

    Update Principal.java

--- src/controller/Principal.java
+++ src/controller/Principal.java
@@ -1,13 +1,14 @@
 package controller;
 
 
+
 public class Principal {	
 
-	
-	public void FPLTesteSuite(String ambiente, String usuario, String senha) {
+	FPLTesteSuite controller;
+	public void executarFPLTesteSuite(String ambiente, String usuario, String senha) {
 		// TODO Auto-generated method stub
 	
-		FPLTesteSuite.abrirURL(ambiente);
+		controller.abrirURL(ambiente);
 	}
 
 
