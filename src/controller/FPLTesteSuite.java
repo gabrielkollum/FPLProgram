79d7fa0f552096dd5d9d34cfa131341832be24b5
gabrielkollum <58408268+gabrielkollum@users.noreply.github.com>
2021-01-26 16:11:42 +0000

    Update FPLTesteSuite.java

--- src/controller/FPLTesteSuite.java
+++ src/controller/FPLTesteSuite.java
@@ -11,14 +11,13 @@ public class FPLTesteSuite {
 	
 	public  FPLTesteSuite() {
 		
-		System.setProperty("webdriver.gecko.driver", "geckodriver");
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		driver = new FirefoxDriver();
 	    	wait = new WebDriverWait(driver, 10);
 	}
 	
 	public  void abrirURL(String ambiente) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		driver.get(ambiente);
 	}
 
